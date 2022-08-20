f2fee9a5c46e5d4761ea8c87cefd6350d66442bd
ossygarcia <mrbrucekessy@gmail.com>
2022-08-20 04:21:19 +0100

    commit

--- controllers/hotelController.js
+++ controllers/hotelController.js
@@ -8,3 +8,49 @@ export const createHotel = async (req, res, next) => {
     next(err);
   }
 };
+
+export const updateHotel = async (req, res, next) => {
+    try {
+        const updateHotel = await hotelModel.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+        );
+        res.status(200).json({ msg: updateHotel });
+      } catch (err) {
+        next(err)
+      }
+  };
+
+  export const getHotel = async (req, res, next) => {
+    const createHotel = new hotelModel(req.body);
+    try {
+      const saveHotel = await createHotel.save();
+      res.status(200).json({ msg: saveHotel });
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  export const getAllHotel = async (req, res, next) => {
+    try {
+        const allHotel = await hotelModel.find();
+        res.status(200).json({ msg: allHotel });
+      } catch (err) {
+        next(err);
+      }
+  };
+
+  export const deleteHotel = async (req, res, next) => {
+    try {
+        await hotelModel.findByIdAndDelete(req.params.id);
+        if (!req.params.id) {
+          res.status(404).send("Record does not exit");
+        }
+        res.status(200).send("Deleted Successfully");
+      } catch (err) {
+       next(err)
+      }
+  };
--- routes/hotels.js
+++ routes/hotels.js
@@ -1,56 +1,26 @@
-import express from 'express';
-import { createHotel } from '../controllers/hotelController.js';
+import express from "express";
+import {
+  createHotel,
+  deleteHotel,
+  getAllHotel,
+  getHotel,
+  updateHotel,
+} from "../controllers/hotelController.js";
+
 const router = express.Router();
 
-//CREATE
+//CREATE ROUTE
 router.post("/", createHotel);
 
-//UPDATE
-router.put("/:id", async (req, res) => {
-  try {
-    const updateHotel = await hotelModel.findByIdAndUpdate(
-      req.params.id,
-      {
-        $set: req.body,
-      },
-      { new: true }
-    );
-    res.status(200).json({ msg: updateHotel });
-  } catch (err) {
-    res.status(500).json({ msg: "Updated Successfully" });
-  }
-});
+//UPDATE ROUTE
+router.put("/:id", updateHotel);
 
-//DELETE
-router.delete("/:id", async (req, res) => {
-  try {
-    await hotelModel.findByIdAndDelete(req.params.id);
-    if (!req.params.id) {
-      res.status(404).send("Record does not exit");
-    }
-    res.status(200).send("Deleted Successfully");
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//DELETE ROUTE
+router.delete("/:id", deleteHotel);
 
-//GET ALL
-router.get("/", async (req, res, next) => {
-  try {
-    const allHotel = await hotelModel.find();
-    res.status(200).json({ msg: allHotel });
-  } catch (err) {
-    next(err);
-  }
-});
+//GET ALL ROUTE
+router.get("/", getAllHotel);
 
-//GET
-router.get("/:id", async (req, res, next) => {
-  try {
-    const allHotel = await hotelModel.findById("fjfjru");
-    res.status(200).json({ msg: allHotel });
-  } catch (err) {
-    next(err);
-  }
-});
-export default router
+//GET ROUTE
+router.get("/:id", getHotel);
+export default router;
